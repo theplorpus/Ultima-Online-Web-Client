647e840919929b17b810e0453c328138da88dd54
Nick McCormack <nick@cartmonks.com>
2010-11-01 00:10:53 -0500

    Updated loader.php to scan for only js files.
    Signed-off-by: Nick McCormack <nick@cartmonks.com>

--- php/loader.php
+++ php/loader.php
@@ -2,9 +2,12 @@
 	/*****************************************************************\
 	 * One loader to bring them all and in the darkness - bind them! *
 	\*****************************************************************/
-	
+
 	// Returns a JSON array of all scripts that the client uses
 	function loadScripts( $path = '.', $level = 0 ){ 
+
+		// Output array
+		$return;
 
     // Directories to ignore when listing output. Many hosts 
     // will deny PHP access to the cgi-bin. 
@@ -18,28 +21,32 @@
 
 				// Check that this file is not to be ignored 
         if( !in_array( $file, $ignore ) ){ 
-             
-						// Just to add spacing to the list, to better 
-            // show the directory tree. 
-            $spaces = str_repeat( '&nbsp;', ( $level * 4 ) ); 
-            
-						// Its a directory, so we need to keep reading down...
-            if( is_dir( "$path/$file" ) ){ 
 
-								// Re-call this same function but on a new directory. 
-                // this is what makes function recursive. 
-                echo "<strong>$spaces $file</strong><br />"; 
-                getDirectory( "$path/$file", ($level+1) ); 
-             
-            } else { 
-						
-                // Just print out the filename 
-                echo "$spaces $file<br />"; 
-             
-            } 
+						// Get the pathinfo
+						$pathparts = pathinfo($path.'/'.$file);
+
+						// Check that this file is a .js file
+						if( $pathparts['extension'] == 'js' ) {
+
+								// Its a directory, so we need to keep reading down...
+								if( is_dir( "$path/$file" ) ){ 
+
+										// Recurse on a new directory. 
+										getDirectory( "$path/$file", ($level+1) ); 
+
+								} else { 
+
+										// Add to the array
+										$return[] = $file;
+
+								} 
+						}
         } 
     } 
-		
+
     // Close the directory handle 
     closedir( $dh ); 
+
+		// Encode to JSON and return
+		return json_encode($return);
 } 
